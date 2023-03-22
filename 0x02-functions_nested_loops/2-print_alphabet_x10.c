ebee1f5c1e01cc762e748ae16a67b70d1ead2145
Samuel Munara <munarasamuel@gmail.com>
2023-03-22 05:46:03 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - not defined
+ *print_alphabet_x10 - Prints the alphabet 10 times.
+ *Return: always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = i; i <= 10; i++)
+	{
+		putchar(i);
+		for (j = 97; j <= 122; j++)
+		{
+			putchar(j);
+		}
+	}
+}
