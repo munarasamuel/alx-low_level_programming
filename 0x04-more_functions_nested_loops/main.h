70a9eeb2b321868023a826a1b07f174aad941cd9
Samuel Munara <munarasamuel@gmail.com>
2023-03-25 06:33:10 -0700

    function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,10 +5,11 @@
 *Author - Samuel Munara
 *Description: header contains prototypes for all files in this project
 */
-int_putchar(char c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif
