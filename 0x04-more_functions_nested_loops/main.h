8b0e8b39b635ec0843bc4d47ef6fd695c3249d19
Samuel Munara <munarasamuel@gmail.com>
2023-03-25 05:50:50 -0700

    unction that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,8 +5,10 @@
 *Author - Samuel Munara
 *Description: header contains prototypes for all files in this project
 */
+int_putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
