6543c3090cb871d040a68bfc599a2c535ce0c5a1
Samuel Munara <munarasamuel@gmail.com>
2023-04-09 12:36:40 -0700

    a program that prints all arguments it receives.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,7 +8,11 @@
 */
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
