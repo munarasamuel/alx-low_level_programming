dc762e5ccaededfd064b88b4aecbaaa2a002344e
Samuel Munara <munarasamuel@gmail.com>
2023-04-14 08:04:11 -0700

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *_memset(char *s, char b, unsigned int n);
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
